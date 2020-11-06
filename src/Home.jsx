c5cb3bda9c3bee4d0b92c50f0fd59c7fdc937a26
Kyungin Na <KyunginNa@1.local>
2020-11-06 12:27:34 +0100

    added background color and styled Home section.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { Container, Item } from "semantic-ui-react";
-
-const paragraph = `My name is Kyungin Na, a web developer based in Stockholm, Sweden. I'm
-          currently studying at Craft Academy to start a career as a full stack
-          web developer. On this website, you can find some brief information
-          about me and the projects that I have worked on.`;
+import "./home.css";
 
 const Home = () => {
   return (
-      <Container>
+    <Container id="welcome">
+      <Item.Group>
         <Item>
-          <Item.Image size="small" src="./photo/grad.png" />
-          <Item.Content>
-            <Item.Header>Hi!😊</Item.Header>
-            <Item.Description>{paragraph}</Item.Description>
+          {/* <Item.Image size="medium" src="./photo/grad.png" /> */}
+          <Item.Content verticalAlign="middle" text>
+            <Item.Header as="a">
+              <h1>Hi!😃</h1>
+            </Item.Header>
+            <Item.Description>
+              <h2>
+                My name is Kyungin Na, <br />a web developer based in Stockholm,
+                Sweden.
+              </h2>
+              <p>
+                I'm currently studying at Craft Academy to start a career as a
+                full stack web developer.
+              </p>
+              <p>
+                On this website, you can find some brief information about me
+                and the projects that I have worked on.
+              </p>
+            </Item.Description>
           </Item.Content>
         </Item>
-      </Container>
+      </Item.Group>
+    </Container>
   );
 };
 
