f446b25b97b7a8ddb3016b44fd5e279468746811
Kyungin Na <KyunginNa@1.local>
2020-11-07 19:00:36 +0100

    Created BasciInfo component that contains basic info about me, added styling using Grid and Icon component, and modified a test accordingly.

--- src/About.jsx
+++ src/About.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Container, Item } from "semantic-ui-react";
 import BasicInfo from "./BasicInfo";
-import Skills from "./Skills"
-import Hobby from "./Hobby"
+import Skills from "./Skills";
+import Hobby from "./Hobby";
 
 const About = () => {
   return (
     <Container>
-      <h1 id="about-header">About Me</h1>
-      <Item.Group relaxed>
-        <BasicInfo />
+      <BasicInfo />
+      <Item.Group>
         <Skills />
         <Hobby />
       </Item.Group>
--- src/BasicInfo.jsx
+++ src/BasicInfo.jsx
@@ -1,27 +1,38 @@
 import React from "react";
-import { Item } from "semantic-ui-react";
+import { Image, List, Grid } from "semantic-ui-react";
 
 const BasicInfo = () => {
   return (
-    <Item>
-      <Item.Image size="small" src="photo/grad.png" />
-      <Item.Content verticalAlign="middle">
-        <Item.Description>
-          <p>
-            I'm a web developer living in Stockholm, Sweden. I'm currently
-            studying at Craft Academy to be a 'great' full stack web developer.
-          </p>
-          <p>
-            I have a passion in creating things and solving problems in logical
-            ways, which explains why I love programming.
-          </p>
-          <p>
-            I aim to be a programmer who communicates well with teammates, and
-            also with clients.
-          </p>
-        </Item.Description>
-      </Item.Content>
-    </Item>
+    <div>
+      <Grid style={{ marginTop: "-1.5em" }}>
+        <Grid.Row centered verticalAlign="middle">
+          <Grid.Column width={3}>
+            <Image src="photo/grad.png" size="medium" />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <h2 id="about-header">Kyungin Na</h2>
+            <List>
+              <List.Item icon="users" content="Craft Academy" />
+              <List.Item icon="marker" content="Stockholm, Sweden" />
+              <List.Item
+                icon="mail"
+                content={
+                  <a href="mailto:nakyungin93@gmail.com">
+                    nakyungin93@gmail.com
+                  </a>
+                }
+              />
+            </List>
+            <p>
+              I'm currently studying at Craft Academy to be a 'great' full stack
+              web developer. I have a passion in creating things and solving
+              problems in logical ways. I aim to be a programmer who
+              communicates well with teammates, and also with clients.
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
   );
 };
 
