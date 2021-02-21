b61f4feed9b5009a2884726293bfe296be270f7d
Kyungin Na <nakyungin93@gmail.com>
2021-02-21 21:31:19 +0100

    Removing footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./components/Home";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import { Route, Switch } from "react-router-dom";
@@ -16,7 +15,6 @@ const App = () => {
         <Route exact path="/projects" component={Projects}></Route>
         <Home />
       </Switch>
-      <Footer />
     </div>
   );
 };
