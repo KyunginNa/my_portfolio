6e3bebaa49fad3d3ea69c8d795f78283fc5756b3
Kyungin Na <nakyungin93@gmail.com>
2021-02-21 21:32:04 +0100

    Removing footer from cypress test and stylesheet

--- cypress/integration/userCanSeePortfolioStructure.feature.js
+++ cypress/integration/userCanSeePortfolioStructure.feature.js
@@ -2,7 +2,6 @@ describe('Portfolio interface', () => {
   it('successfully renders', () => {
     cy.visit('http://localhost:3000/')
     cy.get('#header').should('contain', 'Home')
-    cy.get('#footer').should('contain', 'Made with React 17.0.1')
     cy.get('#home').should('contain', 'Welcome!')
   })
 })
