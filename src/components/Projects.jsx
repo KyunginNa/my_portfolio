a671f005505a9913575e4320c520a5353d90c41e
Kyungin Na <nakyungin93@gmail.com>
2021-02-22 15:01:01 +0100

    Adding vertical menu bar in projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid } from "semantic-ui-react";
+import { Grid, Menu } from "semantic-ui-react";
 import axios from "axios";
 import ProjectCard from "./ProjectCard";
 
@@ -23,10 +23,45 @@ class Projects extends Component {
       );
     });
     return (
-      <div style={{ margin: 30 }}>
-        <h1 data-cy="projects-header" style={{ textAlign: 'center', marginBottom: "1.5em" }}>My Projects</h1>
-        <Grid centered>{projectsList}</Grid>
-      </div>
+      <>
+        <Menu vertical style={{ backgroundColor: 'rgba(255,255,255,0.8)' }}>
+          <Menu.Item>
+            <Menu.Header>Web Applications</Menu.Header>
+            <Menu.Menu>
+              <Menu.Item
+                name='kitty news web'
+              />
+              <Menu.Item
+                name='K-Food'
+              />
+              <Menu.Item
+                name='Rock Paper Scissors'
+              />
+              <Menu.Item
+                name='Trumpster'
+              />
+              <Menu.Item
+                name='Cooper Test'
+              />
+            </Menu.Menu>
+          </Menu.Item>
+          <Menu.Item>
+            <Menu.Header>Mobile Applications</Menu.Header>
+            <Menu.Menu>
+              <Menu.Item
+                name='tuneshare'
+              />
+              <Menu.Item
+                name='kitty news mobile'
+              />
+            </Menu.Menu>
+          </Menu.Item>
+        </Menu>
+        <div style={{ margin: 30 }}>
+          <h1 data-cy="projects-header" style={{ textAlign: 'center', marginBottom: "1.5em" }}>My Projects</h1>
+          <Grid centered>{projectsList}</Grid>
+        </div>
+      </>
     );
   }
 }
