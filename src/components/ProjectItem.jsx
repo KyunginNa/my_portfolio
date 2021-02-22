0488462302ee7f006665b9e6d38cb94ec37bc65a
Kyungin Na <nakyungin93@gmail.com>
2021-02-22 21:08:38 +0100

    Setting initial project to be displayed

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -3,30 +3,31 @@ import { Image, Item, Icon, Label } from "semantic-ui-react";
 
 const ProjectItem = ({ project }) => {
   let technologyLabel = project.technologies.map(technology => {
-    return <Label key={project.id} color="teal" size="medium">{technology}</Label>
+    return <Label key={`${project.id}-${technology}`} color="teal" size="medium">{technology}</Label>
   })
+
   return (
     <Item.Group>
       <Item>
-        <Image src={project.image} style={{ height: 300 }} />
-        <Item.Content verticalAlign='middle'>
-          <Item.Header>{project.name}</Item.Header>
-          <Item.Description>{project.description}</Item.Description>
-          <Item.Extra>
-            {technologyLabel}
-            <br />
-            <div style={{ float: 'right' }}>
-              {project.url &&
-                <a href={project.url} target="_blank" rel="noreferrer noopener">
-                  <Icon name="external alternate" id={`urlIcon-${project.id}`} color="teal" size="large" />
+        <Image src={project.image} style={{ height: 300 }} bordered />
+          <Item.Content verticalAlign='middle'>
+            <Item.Header>{project.name}</Item.Header>
+            <Item.Description>{project.description}</Item.Description>
+            <Item.Extra>
+              {technologyLabel}
+              <br />
+              <div style={{ float: 'right' }}>
+                {project.url &&
+                  <a href={project.url} target="_blank" rel="noreferrer noopener">
+                    <Icon name="external alternate" id={`urlIcon-${project.id}`} color="teal" size="large" />
+                  </a>
+                }
+                <a href={project.github} target="_blank" rel="noreferrer noopener">
+                  <Icon name="github" id={`gitIcon-${project.id}`} color="teal" size="large" />
                 </a>
-              }
-              <a href={project.github} target="_blank" rel="noreferrer noopener">
-                <Icon name="github" id={`gitIcon-${project.id}`} color="teal" size="large" />
-              </a>
-            </div>
-          </Item.Extra>
-        </Item.Content>
+              </div>
+            </Item.Extra>
+          </Item.Content>
       </Item>
     </Item.Group>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,94 +1,80 @@
 import React, { Component, createRef } from "react";
-import { Ref, Grid, Menu, Sticky, Rail, Segment, Item } from "semantic-ui-react";
+import { Grid, Menu, Segment } from "semantic-ui-react";
 import axios from "axios";
 import ProjectItem from "./ProjectItem";
 
 class Projects extends Component {
   state = {
-    projects: [],
+    project: null,
+    clickedProject: 1,
   };
 
   componentDidMount() {
     axios.get("./data/projects.json").then((response) => {
-      this.setState({ projects: response.data });
+      this.setState({ project: response.data[this.state.clickedProject] });
     });
   }
 
-  contextRef = createRef()
-
   render() {
-    let projectsList = this.state.projects.map((project) => {
-      return (
-        <div id={`project-${project.id}`} key={project.id}>
-          <ProjectItem project={project} />
-        </div>
-      );
-    });
     return (
       <Grid style={{ marginTop: 5, marginLeft: 15 }}>
-        <Grid.Column width={4} style={{ marginTop: 10 }}>
-          <Ref innerRef={this.contextRef}>
-            <Rail style={{ maxWidth: '200px' }}>
-              <Sticky context={this.contextRef}>
-                <Menu vertical style={{ backgroundColor: 'rgba(255,255,255,0.8)' }}>
-                  <Menu.Item>
-                    Web Applications
+        <Grid.Column width={4}>
+          <Menu vertical style={{ backgroundColor: 'rgba(255,255,255,0.8)' }}>
+            <Menu.Item>
+              Web Applications
                     <Menu.Menu>
-                      <Menu.Item
-                        name='kitty news web'
-                      >
-                        Kitty News Web
+                <Menu.Item
+                  name='kitty news web'
+                >
+                  Kitty News Web
                         </Menu.Item>
-                      <Menu.Item
-                        name='slow food'
-                      >
-                        Slow Food
+                <Menu.Item
+                  name='slow food'
+                >
+                  Slow Food
                       </Menu.Item>
-                      <Menu.Item
-                        name='rock paper scissors'
-                      >
-                        Rock Paper Scissors
+                <Menu.Item
+                  name='rock paper scissors'
+                >
+                  Rock Paper Scissors
                       </Menu.Item>
-                      <Menu.Item
-                        name='trumpster'
-                      >
-                        Trumpster
+                <Menu.Item
+                  name='trumpster'
+                >
+                  Trumpster
                       </Menu.Item>
-                      <Menu.Item
-                        name='cooper test'
-                      >
-                        Cooper Test
+                <Menu.Item
+                  name='cooper test'
+                >
+                  Cooper Test
                       </Menu.Item>
-                    </Menu.Menu>
-                  </Menu.Item>
-                  <Menu.Item>
-                    Mobile Applications
+              </Menu.Menu>
+            </Menu.Item>
+            <Menu.Item>
+              Mobile Applications
                     <Menu.Menu>
-                      <Menu.Item
-                        name='tuneshare'
-                      >
-                        TuneShare
+                <Menu.Item
+                  name='tuneshare'
+                >
+                  TuneShare
                       </Menu.Item>
-                      <Menu.Item
-                        name='kitty news mobile'
-                      >
-                        Kitty News Mobile
+                <Menu.Item
+                  name='kitty news mobile'
+                >
+                  Kitty News Mobile
                       </Menu.Item>
-                    </Menu.Menu>
-                  </Menu.Item>
-                </Menu>
-              </Sticky>
-            </Rail>
-          </Ref>
+              </Menu.Menu>
+            </Menu.Item>
+          </Menu>
         </Grid.Column>
-        <Grid.Column width={13} style={{ marginLeft: -120, marginTop: -4 }}>
+        <Grid.Column width={13} style={{ marginLeft: -120 }}>
 
           {/* <div style={{ margin: 30 }}>
               <h1 data-cy="projects-header" style={{ textAlign: 'center', marginBottom: "1.5em" }}>My Projects</h1>
               <Grid centered>{projectsList}</Grid>
             </div> */}
-          <Segment raised style={{ height: 600, backgroundColor: 'rgba(255,255,255,0.8)' }}>
-            {projectsList}
+          <Segment raised style={{ height: 400, backgroundColor: 'rgba(255,255,255,0.7)' }} >
+            {this.state.project && <ProjectItem project={this.state.project} />}
           </Segment>
         </Grid.Column>
       </Grid >
